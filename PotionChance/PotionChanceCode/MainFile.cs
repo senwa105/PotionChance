c15e513090d26c6a725a944c7c6eb5a9486b818c
senwa105 <senwa105@gmail.com>
2026-05-11 08:37:47 -0400

    first commit

---
+++ PotionChance/PotionChanceCode/MainFile.cs
@@ -0,0 +1,22 @@
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
+
+namespace PotionChance.PotionChanceCode;
+
+//You're recommended but not required to keep all your code in this package and all your assets in the PotionChance folder.
+[ModInitializer(nameof(Initialize))]
+public partial class MainFile : Node
+{
+    public const string ModId = "PotionChance"; //At the moment, this is used only for the Logger and harmony names.
+
+    public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
+        new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
+
+    public static void Initialize()
+    {
+        Harmony harmony = new(ModId);
+
+        harmony.PatchAll();
+    }
+}
