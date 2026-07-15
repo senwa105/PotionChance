8c3f21baf8ac9fafbc2f10e31348c0b81dd52376
senwa105 <senwa105@gmail.com>
2026-07-15 17:14:47 -0400

    refactor to not use AbstractModel

--- PotionChanceCode/PotionChanceTracker.cs
+++ PotionChanceCode/PotionChanceTracker.cs
@@ -1,6 +1,5 @@
 using MegaCrit.Sts2.Core.Context;
 using MegaCrit.Sts2.Core.Entities.Players;
-using MegaCrit.Sts2.Core.Modding;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Relics;
 using MegaCrit.Sts2.Core.Runs;
@@ -8,12 +7,17 @@ using MegaCrit.Sts2.Core.Rooms;
 
 namespace PotionChance.PotionChanceCode;
 
-public class PotionChanceTracker : SingletonModel
+public class PotionChanceTracker : IDisposable
 {
+    private static bool _initialized = false;
+    
+    public static PotionChanceTracker? Instance { get; private set; }
+    
     private Player? _player;
     
     private Player Player => _player ?? throw new InvalidOperationException("Player accessed before initialization.");
-    
+
+    private RunState _runState;
     public event Action<float, bool>? ChanceUpdated;
 
     public bool HasEliteBonus { get; private set; }
@@ -31,26 +35,43 @@ public class PotionChanceTracker : SingletonModel
             return Math.Min(1f, Chance + eliteBonus + wbsBonus);
         }
     }
-
-    public override bool ShouldReceiveCombatHooks => true;
-
-    public PotionChanceTracker()
+    
+    public static void Initialize()
     {
-        ModHelper.SubscribeForRunStateHooks(Id.Entry, RunSubModels);
+        if (_initialized) return;
         
         RunManager.Instance.RunStarted += OnRunStarted;
+        
+        _initialized = true;
+        MainFile.Logger.Info("PotionChanceTracker initialized.");
+    }
+
+    private static void OnRunStarted(RunState runState)
+    {
+        if (Instance != null)
+        {
+            MainFile.Logger.Info("Disposing previous PotionChanceTracker instance on run resume.");
+            Instance.Dispose(); 
+        }
+        
+        Instance = new PotionChanceTracker(runState);
+        MainFile.Logger.Info("New PotionChanceTracker constructed.");
+    }
+
+    private PotionChanceTracker(RunState runState)
+    {
+        RunManager.Instance.RoomEntered += OnRoomEntered;
+        
         PotionOddsEvents.PotionRolled += OnPotionRolled;
         PotionOddsEvents.OddsOverridden += OnOddsOverridden;
+
+        _runState = runState;
+        InitPlayer();
     }
-    
-    private IEnumerable<AbstractModel> RunSubModels(RunState runState)
+
+    private void InitPlayer()
     {
-        return [this];
-    }
-    
-    private void OnRunStarted(RunState runState)
-    {
-        Player? me = LocalContext.GetMe(runState.Players);
+        Player? me = LocalContext.GetMe(_runState.Players);
         if (me == null)
         { 
             MainFile.Logger.Warn("PotionChanceTracker failed to get player.");
@@ -62,9 +83,7 @@ public class PotionChanceTracker : SingletonModel
         Player.RelicRemoved += OnRelicRemoved;
         
         Chance = Player.PlayerOdds.PotionReward.CurrentValue;
-        MainFile.Logger.Info($"Chance initialized to {Chance}.");
-        
-        HasEliteBonus = runState.CurrentRoom?.RoomType == RoomType.Elite;
+        HasEliteBonus = _runState.CurrentRoom?.RoomType == RoomType.Elite;
         HasWhiteBeastStatue = Player.Relics.Any(r => r is WhiteBeastStatue);
         
         ChanceUpdated?.Invoke(TotalChance, HasEliteBonus || HasWhiteBeastStatue);
@@ -105,11 +124,22 @@ public class PotionChanceTracker : SingletonModel
         ChanceUpdated?.Invoke(TotalChance, HasEliteBonus || HasWhiteBeastStatue);
     }
 
-    public override Task BeforeRoomEntered(AbstractRoom room)
+    private void OnRoomEntered()
     {
-        HasEliteBonus = room.RoomType == RoomType.Elite;
+        HasEliteBonus = _runState.CurrentRoom!.RoomType == RoomType.Elite;
         ChanceUpdated?.Invoke(TotalChance, HasEliteBonus || HasWhiteBeastStatue);
+    }
+    
+    public void Dispose()
+    {
+        PotionOddsEvents.PotionRolled -= OnPotionRolled;
+        PotionOddsEvents.OddsOverridden -= OnOddsOverridden;
+        RunManager.Instance.RoomEntered -= OnRoomEntered;
         
-        return Task.CompletedTask;
+        if (_player != null)
+        {
+            _player.RelicObtained -= OnRelicObtained;
+            _player.RelicRemoved -= OnRelicRemoved;
+        }
     }
 }
